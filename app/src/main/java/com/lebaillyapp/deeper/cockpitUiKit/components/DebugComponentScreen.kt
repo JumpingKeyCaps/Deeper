2e045d93290c951954165681abd019b11d9e5284
Samuel Lebailly <sam.meusa@gmail.com>
2026-05-06 00:24:40 +0200

    feature(cockpitUiKit/components): Tweak the debug component screen to test the DualTapeAltimeter component.

--- app/src/main/java/com/lebaillyapp/deeper/cockpitUiKit/components/DebugComponentScreen.kt
+++ app/src/main/java/com/lebaillyapp/deeper/cockpitUiKit/components/DebugComponentScreen.kt
@@ -2,46 +2,56 @@ package com.lebaillyapp.deeper.cockpitUiKit.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.lebaillyapp.deeper.cockpitUiKit.components.atomic.glassButton.GlassButtonConfig
-import com.lebaillyapp.deeper.cockpitUiKit.components.atomic.glassButton.GlassButtonNode
+import com.lebaillyapp.deeper.cockpitUiKit.components.atomic.dualTapeAltimeter.DualTapeAltimeterConfig
+import com.lebaillyapp.deeper.cockpitUiKit.components.atomic.dualTapeAltimeter.DualTapeAltimeterNode
+import kotlinx.coroutines.delay
 
 @Composable
 fun DebugComponentScreen() {
-    // Instance du bouton
-    val testNode = remember {
-        GlassButtonNode(
-            id = "BTN_GEAR",
-            config = GlassButtonConfig(
-                titleParam = "GEAR DOWN",
-                colorParam = Color(0xFFF84367), // Vert cockpit
-                width = 90.dp,
-                height = 30.dp,
-                cornerRadius = 8.dp,
-                glowRadius = 4.9f
+    val depthNode = remember {
+        DualTapeAltimeterNode(
+            id = "DEPTH_INDICATOR",
+            config = DualTapeAltimeterConfig(
+                titleParam = "DEPTH",
+                minAltitude = -30000f,
+                maxAltitude = 0f,
+                majorTickEvery = 10,
+                minorTickEvery = 2,
+                centerFontSize = 30f,
+                recordDepthParam = -124.7f // On place un record à -100m
             )
         )
+    }
+
+    var simuDepth by remember { mutableStateOf(0f) }
+
+    LaunchedEffect(Unit) {
+        while (true) {
+            delay(16)
+            simuDepth += 0.5f // Descente
+
+            val finalValue = if (simuDepth == 0f) 0f else -simuDepth
+            depthNode.updateAltitude(finalValue)
+
+            // Si on dépasse -20000, on reset pour le test
+            if (simuDepth > 1250f) simuDepth = 0f
+        }
     }
 
     Box(
         modifier = Modifier.fillMaxSize().background(Color.Black),
         contentAlignment = Alignment.Center
     ) {
-        // Rendu du bouton
-        testNode.Render(modifier = Modifier.wrapContentSize())
-
-        // Bouton de simulation (Moteur de jeu)
-        Button(
-            onClick = { testNode.state.update(!testNode.state.value) },
-            modifier = Modifier.align(Alignment.BottomCenter).padding(bottom = 50.dp)
-        ) {
-            Text(if (testNode.state.value) "SYSTEM: ON" else "SYSTEM: OFF")
-        }
+        depthNode.Render(
+            modifier = Modifier
+                .width(180.dp)
+                .height(220.dp)
+                .padding(3.dp)
+        )
     }
 }
